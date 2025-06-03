12286102f98bd843d19d7732ad06f258a657c3cc
danilpovarov <danil.povarov@tele2.kz>
2025-06-03 10:39:36 +0500

    from Andreys rar file

--- static/scripts/favourites.js
+++ static/scripts/favourites.js
@@ -36,15 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const cardsData = {
-    "1": { id: "1", title: "Бег", img: "/static/img/image 3.png", category: "sports", participants: 25, url: "topics/running.html" },
-    "2": { id: "2", title: "Собаки", img: "/static/img/image 4.png", category: "animals", participants: 135, url: "topics/dogs.html" },
-    "3": { id: "3", title: "Кошки", img: "/static/img/image 5.png", category: "animals", participants: 775, url: "topics/cats.html" },
-    "4": { id: "4", title: "Футбол", img: "/static/img/image.png", category: "sports", participants: 2000, url: "topics/soccer.html" },
-    "5": { id: "5", title: "Велоспорт", img: "../static/img/image (1).png", category: "sports", participants: 1529, url: "/templates/cycling" },
-    "6": { id: "6", title: "Кемпинг", img: "/static/img/image (2).png", category: "travel", participants: 245, url: "topics/camping.html" },
-    "7": { id: "7", title: "Хайкинг", img: "/static/img/hiking.jpg", category: "travel", participants: 91, url: "topics/hiking.html" },
-    "8": { id: "8", title: "Боевики", img: "/static/img/boevik.png", category: "movies", participants: 460, url: "topics/boeviki.html" },
-    "9": { id: "9", title: "Фантастика", img: "/static/img/scifi.png", category: "movies", participants: 513, url: "topics/scifi.html" },
+    "1": { id: "1", title: "Бег", img: "/static/img/image 3.png", category: "sports", participants: 25, url: "running.html" },
+    "2": { id: "2", title: "Собаки", img: "/static/img/image 4.png", category: "animals", participants: 135, url: "dogs.html" },
+    "3": { id: "3", title: "Кошки", img: "/static/img/image 5.png", category: "animals", participants: 775, url: "cats.html" },
+    "4": { id: "4", title: "Футбол", img: "/static/img/image.png", category: "sports", participants: 2000, url: "soccer.html" },
+    "5": { id: "5", title: "Велоспорт", img: "/static/img/image (1).png", category: "sports", participants: 1529, url: "cycling.html" },
+    "6": { id: "6", title: "Кемпинг", img: "/static/img/image (2).png", category: "travel", participants: 245, url: "camping.html" },
+    "7": { id: "7", title: "Хайкинг", img: "/static/img/hiking.jpg", category: "travel", participants: 91, url: "hiking.html" },
+    "8": { id: "8", title: "Боевики", img: "/static/img/boevik.png", category: "movies", participants: 460, url: "boeviki.html" },
+    "9": { id: "9", title: "Фантастика", img: "/static/img/scifi.png", category: "movies", participants: 513, url: "scifi.html" },
   };
 
   temkiContainer.innerHTML = '';
